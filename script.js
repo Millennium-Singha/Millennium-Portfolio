8d151a6118b836489d3ecbbfc41e26ff4930ca05
Millennium Singha <millenniumsingha2000@gmail.com>
2024-02-15 17:11:34 +1000

    Changed the default state of site to dark mode

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const switcher = document.getElementById('mode-switcher');
+const body = document.body;
+const isDarkMode = body.classList.contains('dark-mode');
+
+// Check if the switcher should be in the 'dark-mode' state
+const isSwitcherDarkMode = switcher.classList.contains('dark-mode');
+if (isDarkMode && !isSwitcherDarkMode) {
+    switcher.classList.add('dark-mode');
+}
+
 switcher.addEventListener('click', function() {
-    const body = document.body;
     const isLightMode = body.classList.contains('light-mode');
     body.classList.toggle('light-mode', !isLightMode);
     body.classList.toggle('dark-mode', isLightMode);
